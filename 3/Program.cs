febaaac7eac068276788edc15a928f2ad1defa6c
Lera <miss_val@bk.ru>
2022-12-07 18:28:23 +0300

    Задача 29

---
+++ 3/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+int [] newArray = new int [8];
+for (int i = 0; i < 8; i++)
+{
+    Console.WriteLine("Введите число: ");
+    newArray[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine($"{string.Join(", ", newArray)}");
