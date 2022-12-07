feae28e4db4ec75a804745d5f846744618517317
Lera <miss_val@bk.ru>
2022-12-07 18:27:44 +0300

    Задача 27

---
+++ 2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int i = 0;
+int m = n;
+while (m > 0)
+{
+    int q = m % 10;
+    i = i + q;
+    m = (m - q) / 10;
+}
+
+Console.WriteLine($"Сумма цифр: {i}");
