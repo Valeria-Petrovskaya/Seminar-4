eb9c9d1272d2d7938317cd16dbac9d4ad8aa95f3
Lera <miss_val@bk.ru>
2022-12-07 18:27:06 +0300

    Задача 25

---
+++ 1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+int resultNumber = 1;
+
+while (B > 0)
+{
+    resultNumber = resultNumber * A;
+    B = B - 1;
+}
+
+Console.WriteLine($"Result: {resultNumber}");
